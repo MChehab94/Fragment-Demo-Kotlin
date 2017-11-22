a68aaf8b82e8b6030f714d09dd6d4189a4d7ba6b
Mohamad <lemirmohammad.shehab@lau.edu>
2017-11-22 17:38:22 +0200

    Added UI for fragment

--- app/src/main/java/mchehab/com/fragmentdemo/HelloFragment.kt
+++ app/src/main/java/mchehab/com/fragmentdemo/HelloFragment.kt
@@ -1,10 +1,13 @@
 package mchehab.com.fragmentdemo
 
+import android.app.AlertDialog
 import android.app.Fragment
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
 
 
 class HelloFragment : Fragment() {
@@ -15,8 +18,20 @@ class HelloFragment : Fragment() {
     }
 
     override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        // Inflate the layout for this fragment
-        return inflater!!.inflate(R.layout.fragment_hello, container, false)
+        val view = inflater!!.inflate(R.layout.fragment_hello, container, false)
+        val editText = view.findViewById<EditText>(R.id.editText)
+        val button = view.findViewById<Button>(R.id.button)
+
+        button.setOnClickListener {
+            AlertDialog.Builder(activity)
+                    .setTitle("Button Clicked")
+                    .setMessage(editText.text.toString())
+                    .setPositiveButton("Dismiss", null)
+                    .create()
+                    .show()
+        }
+
+        return view
     }
 
     companion object {
